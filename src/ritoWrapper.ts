65c90111dcb919e8f75e772dbf25c5641b21f0c5
Luis NIETO MUNOZ <lnieto-m@student.42.fr>
2022-06-16 15:34:19 +0200

    added participants data (WIP: Missing runes logo)

--- src/ritoWrapper.ts
+++ src/ritoWrapper.ts
@@ -1,6 +1,7 @@
+import axios from "axios";
 import { LolApi } from "twisted";
 import { Regions } from "twisted/dist/constants";
-import { ApiResponseDTO, ChampionsDataDragon, CurrentGameInfoDTO, SpectatorNotAvailableDTO } from "twisted/dist/models-dto";
+import { ApiResponseDTO, ChampionsDataDragonDetails, CurrentGameInfoDTO, CurrentGameParticipantDTO, SpectatorNotAvailableDTO, SummonerLeagueDto } from "twisted/dist/models-dto";
 
 interface GameData {
     gameMode: string;
@@ -17,16 +18,33 @@ interface FilteredBannedChampionData extends FilteredChampionData {
     side: number;
 }
 
+interface FilteredParticipantData {
+    summonerName: string;
+    championName: string;
+    championPortraitURL: string;
+    summonerSpells: {
+        spell1URL: string;
+        spell2URL: string;
+    },
+    perks: {
+        main: string;
+        secondary: string;
+    },
+    rank: SummonerLeagueDto[], //string,
+    side: number
+}
+
 export default class RitoWrapper {
 
     private lolApi: LolApi;
-    private championList: ChampionsDataDragon;
+    private championList: ChampionsDataDragonDetails[];
     private gameVersion: string;
+    private summonerSpells: { [key: string]: any };
 
-    private async _filterChampData(champID: number): Promise<FilteredChampionData>{
+    private _filterChampData(champID: number): FilteredChampionData {
         if (champID === 888) return;
         try {
-            const data = await this.lolApi.DataDragon.getChampion(champID);
+            const data = this.championList.find((item) => item.key == champID.toString());
             return {
                 name: data.name,
                 title: data.title,
@@ -43,12 +61,40 @@ export default class RitoWrapper {
 
     }
 
-    private async _filterBannedChampData(champID: number, side: number): Promise<FilteredBannedChampionData> {
+    private _filterBannedChampData(champID: number, side: number): FilteredBannedChampionData {
         if (champID === 888) return;
-        const data = await this._filterChampData(champID);
+        const data = this._filterChampData(champID);
         return {
             ...data,
             side: side,
+        }
+    }
+
+    private async _filterParticipantData(rawData: CurrentGameParticipantDTO, region: Regions): Promise<FilteredParticipantData> {
+        
+        try {
+            const championData = this.championList.find((item) => item.key == rawData.championId.toString());
+            const summonerSpellsList = Object.values(this.summonerSpells.data as { [key: string]: any });
+            const rank = (await this.lolApi.League.bySummoner(rawData.summonerId, region)).response;
+            
+            return {
+                summonerName: rawData.summonerName,
+                championName: championData.name,
+                championPortraitURL: `https://ddragon.leagueoflegends.com/cdn/${this.gameVersion}/img/champion/${championData.image.full}`,
+                summonerSpells: {
+                    spell1URL: `http://ddragon.leagueoflegends.com/cdn/${this.gameVersion}/img/spell/${summonerSpellsList.find(item => (item.key as string) == rawData.spell1Id.toString()).id as string}.png`,
+                    spell2URL: `http://ddragon.leagueoflegends.com/cdn/${this.gameVersion}/img/spell/${summonerSpellsList.find(item => (item.key as string) == rawData.spell2Id.toString()).id as string}.png`
+                },
+                perks: {
+                    main: "",
+                    secondary: ""
+                },
+                rank: rank,
+                side: rawData.teamId
+            }
+        } catch (e) {
+            console.error(e);
+            return undefined;
         }
     }
 
@@ -61,26 +107,38 @@ export default class RitoWrapper {
 
         try {
             const { response: { id } } = await this.lolApi.Summoner.getByName(summoner, region);
-            this.championList = await this.lolApi.DataDragon.getChampion();
+
+            this.championList = Object.values((await this.lolApi.DataDragon.getChampion()).data);
             this.gameVersion = await this.lolApi.DataDragon.getVersions()[0];
-            console.log(this.championList.data);
+            if (!this.gameVersion)
+            {
+                console.log(this.gameVersion, this.championList.length, id);
+                this.gameVersion = "12.11.1";
+            }
+            this.summonerSpells = await (await axios.get(`https://ddragon.leagueoflegends.com/cdn/${this.gameVersion}/data/en_US/summoner.json`)).data;
 
             const gameResponse = await this.lolApi.Spectator.activeGame(id, region);
+
             if ((gameResponse as SpectatorNotAvailableDTO).message) { return { error: "No active game" } }
+
             const currentGame = (gameResponse as ApiResponseDTO<CurrentGameInfoDTO>).response;
             const map = (await this.lolApi.DataDragon.getMaps()).find((item) => item.mapId == currentGame.mapId.toString());
-    
+            
+            // Bans
             const bansPromises: Array<Promise<FilteredBannedChampionData>> = [];
-            for (let champion of currentGame.bannedChampions) { bansPromises.push(this._filterBannedChampData(champion.championId, champion.teamId)); }
+            let bans: FilteredBannedChampionData[] = [];
+            for (let champion of currentGame.bannedChampions) { bans.push(this._filterBannedChampData(champion.championId, champion.teamId)); }
+
+            //Participants
+            let participantsPromises: Promise<FilteredParticipantData>[]= [];
+            for (let participant of currentGame.participants) {participantsPromises.push( this._filterParticipantData(participant, region)); }
+
             return {
                 map: map,
-                mode: currentGame.gameMode,
+                mode: currentGame.gameType,
                 startTime: currentGame.gameStartTime,
-                picks: currentGame.participants,
-                participant: {
-                    summoner
-                },
-                bans: await Promise.all(bansPromises)
+                participants: await Promise.all(participantsPromises),
+                bans: bans
             }
         } catch (e) {
             console.error(e);
@@ -102,18 +160,3 @@ export default class RitoWrapper {
         });
     }
 }
-
-/*{
-    bans (si draft/ranked) DONE
-    game start time DONE
-    mode DONE ?
-    map  DONE
-    par champ: {
-        Summoner name
-        Champ Name
-        portrait
-        runes (principale/secondaire)
-        summoner spells
-        rank soloqueue
-    }
-}*/
